a902c282f8529094c33a4d512dd159b3a5c01701
EdValderramos24 <eduard.valderramos.vasquez@est.una.ac.cr>
2020-10-23 18:12:41 -0600

    Agregar programa para agregar maravillas en Egipto

--- maravillas_grecia.py
+++ maravillas_grecia.py
@@ -1,7 +1,5 @@
 # Recorrido e impresión de las líneas de un archivo de texto
 with open("maravillas_antiguas.csv", "r") as archivo:
     for linea in archivo:
-        print(linea, end='')
-
-
-      
+        if (linea.find ("Egipto") != -1):
+            print(linea, end='')
