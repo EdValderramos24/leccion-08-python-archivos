3aa4982da6c64708bbd30240c149c3f1729d99a0
EdValderramos24 <eduard.valderramos.vasquez@est.una.ac.cr>
2020-10-23 19:19:56 -0600

    Agregar data

--- maravillas_grecia.py
+++ maravillas_grecia.py
@@ -1,4 +1,7 @@
 # Recorrido e impresión de las líneas de un archivo de texto
+
+subhilera = input ("Ingrese la subhilera que se desea buscar: ")
+
 with open("maravillas_antiguas.csv", "r") as archivo:
     for linea in archivo:
         if (linea.find ("Egipto") != -1):
